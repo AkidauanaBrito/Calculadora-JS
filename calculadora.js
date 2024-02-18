5fd680e6319ce3306ec52cabb2bfd431f6b805ee
AkidauanaBrito <akidauana.oliveira@maisunifacisa.com.br>
2024-02-18 19:30:52 -0300

    Corrigindo o código.

--- calculadora.js
+++ calculadora.js
@@ -1,33 +1,33 @@
-const main = document.querySelector('main')
-const root = document.querySelector(':root')
-const input = document.getElementById('Input')
-const resultInput = document.getElementById('result')
+const main = document.querySelector("main")
+const root = document.querySelector(":root")
+const input = document.getElementById("input")
+const resultInput = document.getElementById("result")
 
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
 document.querySelectorAll('.charKey').forEach(function (charKeyBtn) {
-    charKeyBtn.addEventListener('Click', function (){
+    charKeyBtn.addEventListener("Click", function () {
        const value = charKeyBtn.dataset.value
        input.value += value
     })
 })
 
-document.getElementById('clear').addEventListener('click', function() {
-    input.value = ''
+document.getElementById("clear").addEventListener("click", function() {
+    input.value = ""
     input.focus()
 
 })
 
-input.addEventListener('keydown', function (ev) {
+input.addEventListener("keydown", function (ev) {
     ev.preventDefault()
     if (allowedKeys.includes(ev.key)) {
         input.value += ev.key
         return
     }
-    if (ev.key === 'Backspace') { //excluir um caracter
+    if (ev.key === "Backspace") { //excluir um caracter
         input.value = input.value.slice(0, -1) //cortar um caracter
     }
-    if (ev.key === 'Enter') { //exibir o resultado
+    if (ev.key === "Enter") { //exibir o resultado
         calculate()
     }
 })
