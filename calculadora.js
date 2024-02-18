c3c8bd16029ac65e479ce07b4d9652eab058ef57
AkidauanaBrito <akidauana.oliveira@maisunifacisa.com.br>
2024-02-18 19:08:53 -0300

    Adicionando evento a todos os botões da calculadora.

--- calculadora.js
+++ calculadora.js
@@ -5,6 +5,13 @@ const resultInput = document.getElementById('result')
 
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
+document.querySelectorAll('.charKey').forEach(function (charKeyBtn) {
+    charKeyBtn.addEventListener('Click', function ({
+       const value = charKeyBtn.dataset.value
+       input.value += value
+    })
+})
+
 input.addEventListener('keydown', function (ev) {
     ev.preventDefault()
     if (allowedKeys.includes(ev.key)) {
