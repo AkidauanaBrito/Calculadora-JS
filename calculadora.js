227f858a10701523a34100ab59da934f86ff7bf3
AkidauanaBrito <akidauana.oliveira@maisunifacisa.com.br>
2024-02-18 12:38:17 -0300

    Inserindo função para impedir a entrada de letras que não estejam na lista permitida.

--- calculadora.js
+++ calculadora.js
@@ -4,3 +4,12 @@ const input = document.getElementById('Input')
 const resultInput = document.getElementById('result')
 
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+
+input.addEventListener('keydown', function (ev) {
+    ev.preventDefault()
+    if (allowedKeys.includes(ev.key)) {
+        input.value += ev.key
+        return
+    }
+
+})
