a2a8fda21660fd14f8d4d05a03778c76d878d3b9
AkidauanaBrito <akidauana.oliveira@maisunifacisa.com.br>
2024-02-18 12:27:01 -0300

    Implementando a funcionalidade 'digitar' na calculadora.

---
+++ calculadora.js
@@ -0,0 +1,4 @@
+const main = document.querySelector('main')
+const root = document.querySelector(':root')
+const input = document.getElementById('Input')
+const resultInput = document.getElementById('result')
