495af042af8e2598c94b7b90ee146c6425953010
AkidauanaBrito <akidauana.oliveira@maisunifacisa.com.br>
2024-02-19 14:34:11 -0300

    Atualizando função 'calculate' para exibir mensagem de erro ao digitar caracters errados.

--- calculadora.js
+++ calculadora.js
@@ -35,6 +35,8 @@ input.addEventListener("keydown", function (ev) {
 document.getElementById('equal').addEventListener('click', calculate)
 
 function calculate() {
+    resultInput.value = 'ERROR'
+    resultInput.classList.add('error')
     const result = eval(input.value)
     resultInput.value = result
 }
