ccce325f6d017a015f3318cbbbc3632970cd9f15
AkidauanaBrito <akidauana.oliveira@maisunifacisa.com.br>
2024-02-19 13:16:59 -0300

    Adicionando evento de click associando uma função para trocar o tema claro/escuro

--- calculadora.js
+++ calculadora.js
@@ -38,3 +38,23 @@ function calculate() {
     const result = eval(input.value)
     resultInput.value = result
 }
+
+document.getElementById('themeSwitcher').addEventListener('click', funtion () { 
+    if (main.dataset.theme === 'dark'){
+        root.style.setProperty('--bg-color', '#f1f5f9')
+        root.style.setProperty('--border-color', '#aaa')
+        root.style.setProperty('--font-color', '#212529')
+        root.style.setProperty('--primary-color', '#26834a')
+        main.dataset.theme = 'light'
+    } else {
+        root.style.setProperty('--bg-color', '#212529')
+        root.style.setProperty('--border-color', '#666')
+        root.style.setProperty('--font-color', '#f1f5f9')
+        root.style.setProperty('--primary-color', '#a43bd4')
+        main.dataset.theme = 'dark'
+
+    }
+
+//verificar o tema atual(dark)
+// Adicionando evento de click associando uma função para trocar o tema claro/escuro
+})
